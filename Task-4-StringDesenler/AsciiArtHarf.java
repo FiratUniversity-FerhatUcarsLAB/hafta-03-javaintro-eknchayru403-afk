5037ea81a1c8814202a38019794a3782f771a510
Hayrunnisa EKİNCİ <eknchayru403@gmail.com>
2025-10-21 17:59:19 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,9 @@
+public class AsciiArtHarf {
+    public static void main ( String [] args ) {
+        char karakterH = 'H';
+        int asciiDegeri = karakterH;
+        System.out.println("Karakter: " + karakterH);
+        System.out.println("ASCII (Ondalık) Değeri: " + asciiDegeri);
+
+    }
+}
