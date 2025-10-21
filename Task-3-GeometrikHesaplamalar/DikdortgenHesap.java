2b099cddce9dffa9492fde76501ad43ea91fd68b
Hayrunnisa EKİNCİ <eknchayru403@gmail.com>
2025-10-21 17:36:17 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,13 @@
+public class DikdortgenHesap {
+    public static void main ( String [] args ){
+        double uzunluk = 10;
+        double genislik = 4.5;
+        double alan = uzunluk * genislik;
+        double cevre = 2 * (uzunluk + genislik);
+        System.out.println("Uzunluk: " + uzunluk);
+        System.out.println("Genişlik: " + genislik);
+        System.out.println("Alan: " + alan);
+        System.out.println("Çevre: " + cevre);
+    }
+
+}
