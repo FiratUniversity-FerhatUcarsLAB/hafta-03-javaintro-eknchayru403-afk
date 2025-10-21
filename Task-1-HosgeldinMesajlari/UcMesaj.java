54ee27f086f9f68bb05e2b4346e03fb24e06ca4a
Hayrunnisa EKİNCİ <eknchayru403@gmail.com>
2025-10-21 15:12:29 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class UcMesaj {
+    public static void main(String[] args) {
+       System.out.println("Java'ya Hoşgeldiniz");
+       System.out.println("Bilgisayar Bilimlerine Hoşgeldiniz");
+       System.out.println("Programlama Eğlencelidir");
+    }
+}
