00f849241e26e802caedc94839dce55ca83fb607
Hayrunnisa EKİNCİ <eknchayru403@gmail.com>
2025-10-21 16:11:10 +0300

    Update IkiIslem.java

--- Task-2-Matematikselislemler/IkiIslem.java
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -1,6 +1,1 @@
-public class IkiIslem {
-    public static void main(String[] args) {
-        int sonuç = 1+2+3+4+5+6+7+8+9;
-        System.out.println("1+2+3+4+5+6+7+8+9 = " + sonuç);
-    }
-}
+
