84018e833dafdaae66bf3673cc25cacc7dd5fe31
Hayrunnisa EKİNCİ <eknchayru403@gmail.com>
2025-10-21 17:50:43 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+public class IsimKarti {
+ public static void main ( String [] args ) {
+         String ad = " Hayrunnisa ";
+         String soyad = " Ekinci ";
+         String ogrNo = " 240541043 ";
+         String bolum = "  Yazılım Mühendisliği ";
+
+         System . out . println (" =================== ") ;
+         System . out . println (" OGRENCI BILGILERI ") ;
+         System . out . println (" =================== ") ;
+         System . out . println ("Ad : " + ad ) ;
+         System . out . println (" Soyad : " + soyad ) ;
+         System . out . println ("No : " + ogrNo ) ;
+         System . out . println (" Bolum : " + bolum ) ;
+         System . out . println (" =================== ") ;
+         }
+ }
