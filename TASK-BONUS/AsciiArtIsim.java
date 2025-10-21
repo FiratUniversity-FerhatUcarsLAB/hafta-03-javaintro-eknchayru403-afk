b71ef7846c496efeba94c5c52df5ff645cb96b0a
Hayrunnisa EKİNCİ <eknchayru403@gmail.com>
2025-10-21 19:23:05 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,24 @@
+public class AsciiArtIsim {
+    public static void main(String[] args) {
+
+        System.out.println("ASCII Sanat: Hayrunnisa");
+        System.out.println("---------------------------------------------------------------------------------");
+
+
+        System.out.println("H   H   AAA   Y   Y   RRRRR   U   U   N   N   N   N   I   SSSSS   AAA");
+
+
+        System.out.println("H   H  A   A   Y Y    R   R   U   U   N N N   N N N   I   S      A   A");
+
+
+        System.out.println("HHHHH  AAAAA    Y     RRRR    U   U   N  N N  N  N N   I    SS   AAAAA");
+
+
+        System.out.println("H   H  A   A    Y     R R     U   U   N   NN  N   NN   I      S  A   A");
+
+
+        System.out.println("H   H  A   A    Y     R  RR   UUUUU   N   N   N   N   I   SSSSS  A   A");
+
+        System.out.println("---------------------------------------------------------------------------------");
+    }
+}
